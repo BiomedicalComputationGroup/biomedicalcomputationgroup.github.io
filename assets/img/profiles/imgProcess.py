d484bb1006e4a4d071df20a425007434edfbe1ea
r6lm <46680486+r6lm@users.noreply.github.com>
2026-07-10 11:50:10 +0000

    Deploying to gh-pages from @ BiomedicalComputationGroup/biomedicalcomputationgroup.github.io@b1ded2b2831f9872e3b503fbcb3e8acd1a7aa93d 🚀

---
+++ assets/img/profiles/imgProcess.py
@@ -0,0 +1,39 @@
+import os, sys, glob
+from PIL import Image, ImageOps
+
+ratio = 1/1
+inverseRatio = 1/ratio
+size = (1024,1024)
+
+infiles = glob.glob('*.png') + glob.glob('*.jpg') + glob.glob('*.jpeg')
+for infile in infiles:
+    if "_cropped" not in infile:
+        try:
+            print(infile)
+            outfile = os.path.splitext(infile)[0] + "_cropped"
+            im = Image.open(infile)
+            fixedIm = ImageOps.exif_transpose(im) # This fixes the orientation, which may otherwise act funny due to metadata
+            fullX,fullY = fixedIm.size
+            if fullX < ratio * fullY:
+                newX = fullX
+                spaceX = 0
+                newY = fullX * inverseRatio
+                spaceY = (fullY - fullX * inverseRatio) / 2
+            elif fullX > ratio * fullY:
+                newX = fullY * ratio
+                spaceX = (fullX - fullY * ratio) / 2
+                newY = fullY
+                spaceY = 0
+            else:
+                newX = fullX
+                spaceX = 0
+                newY = fullY
+                spaceY = 0
+
+            box = (spaceX, spaceY, spaceX + newX , spaceY + newY)
+            croppedIm = fixedIm.crop(box)
+            croppedIm.thumbnail(size)
+            rgbIm = croppedIm.convert('RGB')
+            rgbIm.save(outfile + ".jpg", "JPEG", optimize=True)
+        except IOError:
+            print("cannot create thumbnail for '%s'" % infile)
---
+++ assets/img/stock/imgProcess.py
@@ -0,0 +1,57 @@
+## This code crops an image to the ratio 1/np.sqrt(2) or its inverse. As kwargs, provide the filename and the orientation of the final image.
+
+import os, sys
+import numpy as np
+from PIL import Image
+
+ratio = np.sqrt(2)
+inverseRatio = 1/ratio
+shortSide = 512
+
+infile = sys.argv[1]
+outfile = os.path.splitext(infile)[0] + "_cropped"
+if infile != outfile:
+    try:
+        im = Image.open(infile)
+        fullX,fullY = im.size
+        aspect = sys.argv[2]
+        if aspect == "tall":
+            size = (shortSide, shortSide * ratio)
+            if fullY > ratio * fullX:
+                newX = fullX
+                spaceX = 0
+                newY = fullX * ratio
+                spaceY = (fullY - fullX * ratio) / 2
+            elif fullY < ratio * fullX:
+                newX = fullY * inverseRatio
+                spaceX = (fullX - fullY * inverseRatio) / 2
+                newY = fullY
+                spaceY = 0
+            else:
+                newX = fullX
+                spaceX = 0
+                newY = fullY
+                spaceY = 0
+        elif aspect == "wide":
+            size = (shortSide * ratio, shortSide)
+            if fullX < ratio * fullY * 1.1:
+                newX = fullX
+                spaceX = 0
+                newY = fullX * inverseRatio / 1.1
+                spaceY = (fullY - fullX * inverseRatio / 1.1) / 2
+            elif fullX > ratio * fullY * 1.1:
+                newX = fullY * ratio * 1.1
+                spaceX = (fullX - fullY * ratio * 1.1) / 2
+                newY = fullY
+                spaceY = 0
+            else:
+                newX = fullX
+                spaceX = 0
+                newY = fullY
+                spaceY = 0
+        box = (spaceX, spaceY, spaceX + newX , spaceY + newY)
+        croppedIm = im.crop(box)
+        croppedIm.thumbnail(size)
+        croppedIm.save(outfile + ".jpg", "JPEG", optimize=True)
+    except IOError:
+        print("cannot create thumbnail for '%s'" % infile)
